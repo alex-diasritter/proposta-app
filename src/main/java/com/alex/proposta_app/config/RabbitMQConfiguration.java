741ea6c88b0d20ba42292767c7c0e85042a4452c
AlexRitter1 <ritter.alex@hotmail.com>
2025-07-23 15:01:48 -0300

    RabbitMQConfiguration.java

---
+++ src/main/java/com/alex/proposta_app/config/RabbitMQConfiguration.java
@@ -0,0 +1,45 @@
+package com.alex.proposta_app.config;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfiguration {
+
+    // a configuração da fila também pode ser feita através da interface do RabbitMQ em localhost:15672/
+
+    @Bean
+    public Queue criarFilaPropostaPendenteMsAnaliseCredito() {
+        return QueueBuilder.durable("proposta-pendente.ms-analise-credito").build(); //mesmo q o rabbit caia, msgs serão persistidas
+    }
+
+    @Bean
+    public Queue criarPropostaPendenteFilaMsNotificacao() {
+        return QueueBuilder.durable("proposta-pendente.ms-notificacao").build();
+    }
+
+    @Bean
+    public Queue criarFilaPropostaConcluidaMsProposta() {
+        return QueueBuilder.durable("proposta-concluida.ms-proposta").build();
+    }
+
+    @Bean
+    public Queue criarFilaPropostaConcluidaMsNotificacao() {
+        return QueueBuilder.durable("proposta-concluida.ms-notificacao").build();
+    }
+
+    @Bean
+    public RabbitAdmin criarRabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> inicializarAdmin(RabbitAdmin rabbitAdmin){
+        return e -> rabbitAdmin.initialize();
+    }
+}
