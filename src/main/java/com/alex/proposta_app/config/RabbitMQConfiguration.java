580692cfa868416213e57e33a121627ea4ff48b2
AlexRitter1 <ritter.alex@hotmail.com>
2025-07-23 15:25:28 -0300

    (RabbitMQConfiguration.java) configurando admin, exchanges e bindings

--- src/main/java/com/alex/proposta_app/config/RabbitMQConfiguration.java
+++ src/main/java/com/alex/proposta_app/config/RabbitMQConfiguration.java
@@ -1,8 +1,13 @@
 package com.alex.proposta_app.config;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
@@ -13,13 +18,15 @@ public class RabbitMQConfiguration {
 
     // a configuração da fila também pode ser feita através da interface do RabbitMQ em localhost:15672/
 
+    // --- FILAS ---
+
     @Bean
     public Queue criarFilaPropostaPendenteMsAnaliseCredito() {
         return QueueBuilder.durable("proposta-pendente.ms-analise-credito").build(); //mesmo q o rabbit caia, msgs serão persistidas
     }
 
     @Bean
-    public Queue criarPropostaPendenteFilaMsNotificacao() {
+    public Queue criarFilaPropostaPendenteMsNotificacao() {
         return QueueBuilder.durable("proposta-pendente.ms-notificacao").build();
     }
 
@@ -39,7 +46,46 @@ public class RabbitMQConfiguration {
     }
 
     @Bean
-    public ApplicationListener<ApplicationReadyEvent> inicializarAdmin(RabbitAdmin rabbitAdmin){
+    public ApplicationListener<ApplicationReadyEvent> inicializarAdmin(RabbitAdmin rabbitAdmin) {
         return e -> rabbitAdmin.initialize();
+    }
+
+    //--------------------------------------------------------------------------------------------
+    // --- EXCHANGES E BINDINGS ---
+
+    // --- Evento: Proposta Pendente ---
+
+    @Bean
+    public FanoutExchange criarFanoutExchangePropostaPendente() {
+        return ExchangeBuilder.fanoutExchange("exchangePropostaPendente").build();
+    }
+
+    @Bean
+    public FanoutExchange criarFanoutExchangePropostaConcluida() {
+        return ExchangeBuilder.fanoutExchange("exchangePropostaConcluida").build();
+    }
+
+    @Bean
+    public Binding criarBindingPropostaPendenteMSAnaliseCredito() {
+        return BindingBuilder.bind(criarFilaPropostaPendenteMsAnaliseCredito()).
+                to(criarFanoutExchangePropostaPendente());
+    }
+
+    @Bean
+    public Binding criarBindingPropostaPendenteMSNotificacao() {
+        return BindingBuilder.bind(criarFilaPropostaPendenteMsNotificacao()).
+                to(criarFanoutExchangePropostaPendente());
+    }
+
+    @Bean
+    public Binding criarBindingPropostaConcluidaMSPropostaApp() {
+        return BindingBuilder.bind(criarFilaPropostaConcluidaMsProposta()).
+                to(criarFanoutExchangePropostaConcluida());
+    }
+
+    @Bean
+    public Binding criarBindingPropostaConcluidaMSNotificacao() {
+        return BindingBuilder.bind(criarFilaPropostaConcluidaMsNotificacao()).
+                to(criarFanoutExchangePropostaConcluida());
     }
 }
