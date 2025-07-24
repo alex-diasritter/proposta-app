7dc8d95a2118a1db502d7be7dc4b2c39c9c8b2e8
AlexRitter1 <ritter.alex@hotmail.com>
2025-07-24 13:49:52 -0300

    docuemntando o método notificarRabbitMQ

--- src/main/java/com/alex/proposta_app/service/PropostaService.java
+++ src/main/java/com/alex/proposta_app/service/PropostaService.java
@@ -7,7 +7,6 @@ import com.alex.proposta_app.repository.PropostaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
@@ -30,11 +29,18 @@ public class PropostaService {
 
     public PropostaResponseDTO criar(PropostaRequestDTO requestDTO) {
         var propostaEntity = PropostaMapper.INSTANCE.converteDtoToProposta(requestDTO);
-        repository.save(propostaEntity);
-        notificarRabbitMQ(propostaEntity);
+        repository.save(propostaEntity); //salva com integrada true
+        notificarRabbitMQ(propostaEntity); //caso não consiga mandar pro rabbit salva com integrada false
         return PropostaMapper.INSTANCE.convertetEntityToDto(propostaEntity);
     }
 
+    /**
+     * No Spring Data JPA, o metodo save() tem um comportamento de "upsert" (uma combinação de INSERT e UPDATE).
+     * Se o objeto não tem um identificador (ID nulo): A entidade é considerada nova, e a operação executada é um INSERT.
+     * Se o objeto já tem um identificador: A entidade é considerada existente. O framework tentará mesclar
+     * (merge) as alterações do objeto em memória com o registro correspondente no banco de dados, resultando
+     * em uma instrução UPDATE.
+     */
     private void notificarRabbitMQ(Proposta proposta) {
         try {
             //fluxo normal, rabbit no ar
