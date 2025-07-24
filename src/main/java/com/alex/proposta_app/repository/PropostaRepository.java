a6d320f539f20c6de9b1bdc4ad8aa1c437299b8b
AlexRitter1 <ritter.alex@hotmail.com>
2025-07-24 11:16:16 -0300

    (PropostaRepository) findAllByIntegradaIsFalse()

--- src/main/java/com/alex/proposta_app/repository/PropostaRepository.java
+++ src/main/java/com/alex/proposta_app/repository/PropostaRepository.java
@@ -4,5 +4,12 @@ import com.alex.proposta_app.domain.entity.Proposta;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface PropostaRepository extends CrudRepository<Proposta, Long> {}
+public interface PropostaRepository extends CrudRepository<Proposta, Long> {
+
+    //query derivada.
+    //o próprio spring data jpa cria a query para nós.
+    List<Proposta> findAllByIntegradaIsFalse();
+}
